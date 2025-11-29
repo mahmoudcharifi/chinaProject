d28b9c196de54f39a885dff0729ff7a18930cf1d
mahmoud charifi <mahmoudcharifi1234icloud.com@mahmouds-MacBook-Pro.local>
2025-11-29 15:26:33 +0100

    Initial commit

---
+++ home.js
@@ -0,0 +1,82 @@
+ const villeDiv = document.querySelector(".villeDiv");
+        const villes = document.querySelectorAll(".villes");
+
+        let index = 0;
+        let visibleCount = getVisibleCount(); 
+        const total = villes.length;
+
+        function getVisibleCount() {
+            if (window.innerWidth > 1251) return 3;
+            if (window.innerWidth > 757) return 2;
+            return 1;
+        }
+
+        function updateVisibility() {
+            villes.forEach((v, i) => {
+                if (i >= index && i < index + visibleCount) {
+                    v.style.display = "block";
+                } else {
+                    v.style.display = "none";
+                }
+            });
+        }
+
+        document.querySelector("#prevBtn").addEventListener("click", () => {
+    index += 1; 
+    if (index > total - visibleCount) {
+        index = 0; 
+    }
+    updateVisibility();
+});
+
+document.querySelector("#nextBtn").addEventListener("click", () => {
+    index -= 1; 
+    if (index < 0) {
+        index = total - visibleCount;
+    }
+    updateVisibility();
+});
+
+        window.addEventListener("resize", () => {
+            visibleCount = getVisibleCount();
+            updateVisibility();
+        });
+
+        updateVisibility();
+
+
+
+
+
+
+const playBtns = document.querySelectorAll(".PlayVd");
+const modal = document.getElementById("videoModal");
+const iframe = document.getElementById("youtubeFrame");
+const closeBtn = document.getElementById("closeVideo");
+
+playBtns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        const link = btn.dataset.video;
+
+        if (!link) return;
+
+        const embed = link.replace("watch?v=", "embed/") + "?autoplay=1";
+
+        iframe.src = embed;
+
+        modal.style.display = "flex";
+    });
+});
+
+closeBtn.addEventListener("click", () => {
+    modal.style.display = "none";
+    iframe.src = "";
+});
+
+window.addEventListener("click", (e) => {
+    if (e.target === modal) {
+        modal.style.display = "none";
+        iframe.src = "";
+    }
+});
+
